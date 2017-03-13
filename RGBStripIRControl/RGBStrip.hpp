94b8619bed2354cbf7b82fe22e4d7ddb2193f4dd
Livat Ablyazov <lablyazov@geolife.co.uk>
2017-03-13 22:40:02 +0300

    Added some interface methods
    Ability to set RGB, bounded glow level

--- RGBStripIRControl/RGBStrip.hpp
+++ RGBStripIRControl/RGBStrip.hpp
@@ -14,19 +14,69 @@ public:
     redValue = 0;
     greenValue = 0;
     blueValue = 0;
+
+    maxGlow = 200;
+
+    isOn = false;
   }
 
   ~RGBStrip ();
 
   void SetRGBValues (int red, int green, int blue)
   {
-    if (red != redValue || green != greenValue || blue != blueValue)
+    int constrainedRed = constrain(red, 0, maxGlow);
+    int constrainedGreen = constrain(green, 0, maxGlow);
+    int constrainedBlue = constrain(blue, 0, maxGlow);
+
+    if (constrainedRed != redValue
+      || constrainedGreen != greenValue
+      || constrainedBlue != blueValue)
     {
-      redValue = red;
-      greenValue = green;
-      blueValue = blue;
+      redValue = constrainedRed;
+      greenValue = constrainedGreen;
+      blueValue = constrainedBlue;
+
+      OnRGBValuesChanged();
     }
   }
+
+  void On ()
+  {
+    if (isOn)
+    {
+      return;
+    }
+
+    if (redValue == greenValue == blueValue == 0)
+    {
+      int middleValue = maxGlow/2;
+
+      SetRGBValues(middleValue, middleValue, middleValue);
+    }
+    else
+    {
+      OnRGBValuesChanged();
+    }
+  }
+
+  void Off ()
+  {
+    if (!isOn)
+    {
+      return;
+    }
+
+    
+  }
+
+private:
+  void OnRGBValuesChanged ()
+  {
+    analogWrite(redPin, redValue);
+    analogWrite(greenPin, greenValue);
+    analogWrite(bluePin, blueValue);
+  }
+
 
 private:
   short redPin;
@@ -36,4 +86,8 @@ private:
   int redValue;
   int greenValue;
   int blueValue;
+
+  int maxGlow;
+
+  bool isOn;
 };
