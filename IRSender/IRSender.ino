3c29c00469c54261421e8519824d2edda31c8f52
Livat Ablyazov <lablyazov@geolife.co.uk>
2017-03-26 22:32:00 +0300

    Added receiving

--- IRSender/IRSender.ino
+++ IRSender/IRSender.ino
@@ -1,4 +1,5 @@
 #include <IRremote.h>
+#include "TransmittersCodes.h"
 
 #define IRTRANSMITTER_PIN 3
 #define IRRECEIVER_PIN 2
@@ -6,3 +7,39 @@
 IRrecv irreceiver(IRPIN);
 
 decode_results results;
+
+void setup () {
+
+  irreceiver.enableIRIn();
+
+  Serial.begin(9600);
+}
+
+void loop () {
+
+  checkIRSensor();
+}
+
+#pragma mark - IR Remote Signal
+
+void checkIRSensor () {
+
+  if (irreceiver.decode(&results)) {
+
+    irreceiver.resume();
+
+    processRemoteComand();
+
+    Serial.print ("Value = ");
+    Serial.print (results.value);
+    Serial.print ("\tBits = ");
+    Serial.print (results.bits);
+    Serial.print ("========\n");
+  }
+}
+
+void processRemoteComand () {
+
+  unsigned long value = results.value;
+
+}
